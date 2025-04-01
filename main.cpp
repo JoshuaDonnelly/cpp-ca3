ee2f4837e476235dbbf786c5de97e4789871da50
Joshuadonnelly <joshuadonnelly180@gmail.com>
2025-04-01 14:08:27 +0100

    Inital Code - Menu started

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+
+
+
+    int choice;
+    do {
+        cout << "\n===== Bug Game =====\n";
+        cout << "1. Initialize Bug Board (load data from file) \n";
+        cout << "2. Display all Bugs\n";
+        cout << "3. Find a Bug (given an id)\n";
+        cout << "4. Tap the Bug Board (cause all to move, then fight/eat)
+\n";
+        cout << "5. Find highest, lowest and Average PPG\n";
+        cout << "6. Sort players by PPG or APG\n";
+        cout << "10. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+        switch (choice) {
+            case 1:
+        }
+    }
+}

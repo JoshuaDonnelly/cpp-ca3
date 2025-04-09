7a38cee1de6adc80d8260084af5df3ca09d1c2c5
Joshuadonnelly <joshuadonnelly180@gmail.com>
2025-04-09 21:55:31 +0100

    SFML working with project

---
+++ cmake-build-debug/CMakeFiles/cpp_ca3.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_ca3.dir/src/Board.cpp.obj"
+  "CMakeFiles/cpp_ca3.dir/src/Board.cpp.obj.d"
+  "CMakeFiles/cpp_ca3.dir/src/Crawler.cpp.obj"
+  "CMakeFiles/cpp_ca3.dir/src/Crawler.cpp.obj.d"
+  "CMakeFiles/cpp_ca3.dir/src/gui.cpp.obj"
+  "CMakeFiles/cpp_ca3.dir/src/gui.cpp.obj.d"
+  "bin/cpp_ca3.exe"
+  "bin/cpp_ca3.exe.manifest"
+  "bin/cpp_ca3.pdb"
+  "libcpp_ca3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_ca3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
